040f3daefaed2cdd5d7562cfeb0096bef6e0ca05
YU(James) DU <csjamesdu@gmail.com>
2020-10-27 21:02:23 +1100

    test commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -23,6 +23,7 @@ class SearchBar extends React.Component {
                             type="text"
                             value={this.state.term}
                             onChange={this.onInputChange}
+                            
                         />
                     </div>
                 </form>
